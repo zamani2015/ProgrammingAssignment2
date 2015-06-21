577403c7f7988d43970af18e3a8d8b28438dd386
zamani2015 <zamani@tmsk.uitm.edu.my>
2015-06-21 13:25:55 +0800

    A cachematrix
    B cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object that stores 
+## a numeric vector and cache's its mean. 
 
-## Write a short comment describing this function
+## The first function, makeVector creates a special "vector", which is really a 
+## list containing a function to
+##
+## set the value of the vector
+## get the value of the vector
+## set the value of the mean
+## get the value of the mean
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the mean of the special "vector" created 
+## with the above function. However, it first checks to see if the mean has already 
+## been calculated. If so, it gets the mean from the cache and skips the 
+## computation. Otherwise, it calculates the mean of the data and sets the value 
+## of the mean in the cache via the setmean function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
